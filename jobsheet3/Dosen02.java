77ab13dddc016058980ee91ca68d622dfb9a7fed
azaria amanda <azaryaamanda@gmail.com>
2025-02-27 18:59:38 +0700

    Tugas 1

---
+++ jobsheet3/Dosen02.java
@@ -0,0 +1,21 @@
+public class Dosen02 {
+    public String kode;
+    public String nama;
+    public String jenisKelamin;
+    public int usia;
+    
+    public Dosen02(String kode, String nama, String jenisKelamin, int usia){
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+    void cetakInfo(){
+        System.out.println("----------------------------------------");
+        System.out.println("Kode Dosen    : " + kode);
+        System.out.println("Nama Dosen    : " + nama);
+        System.out.println("Jenis Kelamin : " + jenisKelamin);
+        System.out.println("Usia Dosen    : " + usia);
+    }
+}
+
---
+++ jobsheet3/DosenDemo02.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class DosenDemo02 {
+    public static void main(String[] args) {
+        Scanner sc02 = new Scanner(System.in);
+        int jumlahDosen, usia;
+        String kode, nama, jenisKelamin;
+        System.out.print("Masukkan jumlah dosen: ");
+        jumlahDosen = sc02.nextInt();
+        sc02.nextLine();
+        Dosen02[] arrayOfDosen = new Dosen02[jumlahDosen];
+
+        for (int i = 0; i < jumlahDosen ; i++) {
+            System.out.println("Masukkan Data Dosen ke-" + (i+1));
+            System.out.print("Kode Dosen    : ");
+            kode = sc02.nextLine();
+            System.out.print("Nama Dosen    : ");
+            nama = sc02.nextLine();
+            System.out.print("Jenis Kelamin : ");
+            jenisKelamin = sc02.nextLine();
+            System.out.print("Usia Dosen    : ");
+            usia = sc02.nextInt();
+            
+            arrayOfDosen[i] = new Dosen02(kode, nama, jenisKelamin, usia);
+            sc02.nextLine();
+        }
+        for (Dosen02 dsn : arrayOfDosen) {
+            dsn.cetakInfo();
+        }
+    }
+}
