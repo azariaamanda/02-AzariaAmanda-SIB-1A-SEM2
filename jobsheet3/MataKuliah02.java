2af0f6c770c7805918315fe068b5552a40424541
azaria amanda <azaryaamanda@gmail.com>
2025-02-27 18:04:04 +0700

    Update Percobaan 3

--- jobsheet3/MataKuliah02.java
+++ jobsheet3/MataKuliah02.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class MataKuliah02 {
     public String kode;
     public String nama;
     public int sks;
     public int jumlahJam;
+
+    public MataKuliah02() {}
 
     public MataKuliah02(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
@@ -10,4 +14,27 @@ public class MataKuliah02 {
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
+
+    void tambahData(Scanner sc2){
+        System.out.print("Kode        : ");
+        kode = sc2.nextLine();
+        System.out.print("Nama        : ");
+        nama = sc2.nextLine();
+        System.out.print("SKS         : ");
+        sks = sc2.nextInt();
+        sc2.nextLine();
+        System.out.print("Jumlah Jam  : ");
+        jumlahJam = sc2.nextInt();
+        sc2.nextLine();
+        System.out.println("---------------------------------------");
+    }
+
+    void cetakInfo() {
+        System.out.println("Kode Mata Kuliah: " + this.kode);
+        System.out.println("Nama Mata Kuliah: " + this.nama);
+        System.out.println("SKS             : " + this.sks);
+        System.out.println("Jumlah Jam      : " + this.jumlahJam);
+        System.out.println("---------------------------------------");
+
+    }`
 }
--- jobsheet3/MataKuliahDemo02.java
+++ jobsheet3/MataKuliahDemo02.java
@@ -1,36 +1,36 @@
 import java.util.Scanner;
 public class MataKuliahDemo02 {
+    
     public static void main(String[] args) {
         Scanner sc2 = new Scanner(System.in);
-        MataKuliah02[] arrayOfMataKuliah = new MataKuliah02[3];
+        
+        int jumlah;
+        System.out.print("Masukkan Jumlah Elemen: ");
+        jumlah = sc2.nextInt();
+        sc2.nextLine();
+        
+        MataKuliah02[] arrayOfMataKuliah = new MataKuliah02[jumlah];
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for (int i=0; i < 3; i++) {
+        for (int i=0; i < jumlah; i++) {
             System.out.println("Masukkan Data Mata Kuliah ke-" + (i + 1));
-            System.out.print("Kode        : ");
-            kode = sc2.nextLine();
-            System.out.print("Nama        : ");
-            nama = sc2.nextLine();
-            System.out.print("SKS         : ");
-            dummy = sc2.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam  : ");
-            dummy =sc2.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("---------------------------------------");
-
-            arrayOfMataKuliah[i] = new MataKuliah02(kode, nama, sks, jumlahJam);   
+            arrayOfMataKuliah[i] = new MataKuliah02();
+            arrayOfMataKuliah[i].tambahData(sc2);
         }
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Data Mata Kuliah ke-" + (i + 1));
-            System.out.println("Kode        : " + arrayOfMataKuliah[i].kode);
-            System.out.println("Nama        : " + arrayOfMataKuliah[i].nama);
-            System.out.println("SKS         : " + arrayOfMataKuliah[i].sks);
-            System.out.println("Jumlah Jam  : " + arrayOfMataKuliah[i].jumlahJam);
-            System.out.println("---------------------------------------");
-
-        }
+        System.out.println("\n============================");
+        for (MataKuliah02 matkul : arrayOfMataKuliah){
+            matkul.cetakInfo();
+        }   
     }
 }
+// for (int i = 0; i < 3; i++) {
+//     System.out.println("Data Mata Kuliah ke-" + (i + 1));
+//     System.out.println("Kode        : " + arrayOfMataKuliah[i].kode);
+//     System.out.println("Nama        : " + arrayOfMataKuliah[i].nama);
+//     System.out.println("SKS         : " + arrayOfMataKuliah[i].sks);
+//     System.out.println("Jumlah Jam  : " + arrayOfMataKuliah[i].jumlahJam);
+//     System.out.println("---------------------------------------");
+
+// }
