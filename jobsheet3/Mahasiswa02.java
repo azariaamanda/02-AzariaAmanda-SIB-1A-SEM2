144c87aa41141c06a7a7cc173bc39205840c36e4
azaria amanda <azaryaamanda@gmail.com>
2025-02-27 19:33:44 +0700

    selesai

--- jobsheet3/Mahasiswa02.java
+++ jobsheet3/Mahasiswa02.java
@@ -10,5 +10,22 @@ public class Mahasiswa02 {
         System.out.println("Kelas: " + kelas);
         System.out.println("IPK: " + ipk);
     }
+
+    // public String nama;
+    // public int nim;
+
+    // public Mahasiswa02() {
+    //     this.nama = "TIdak diketahui";
+    //     this.nim = 0;
+    // }
+
+    // public Mahasiswa02(String nama, int nim) {
+    //     this.nama = nama;
+    //     this.nim = nim;
+    // }
+
+
 }
+
+
 
